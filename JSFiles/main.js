af03e062f42ef79adf448cbd46f465fbba69d248
Octenx <jota.idk@icloud.com>
2023-10-15 06:06:47 -0300

    Adding functionalities with jquery plugins

---
+++ JSFiles/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('#formTask').on('submit', function (e) {
+        e.preventDefault(); //Previne que a página não seja recarregada após o submit.
+    })
+
+    $('#phoneNumber').keypress(function (e) {
+        if (isNaN(String.fromCharCode(e.which))) e.preventDefault();
+    });
+    
+    $('#phoneNumber').mask('(00) 00000-0000');
+
+    $('#cpfNumber').mask('000.000.000-0');
+
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            name: {
+                required: true,
+            }, 
+            email: {
+                required: true,
+            }, 
+            phoneNumber: {
+                required: true,
+            },
+            cpfNumber: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+            address: {
+                required: true,
+            }
+        }
+    })
+})
